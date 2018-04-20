dfc784fec31ca5a73da668c420cda5e04d1a956b
Lawrence Miller <ajacs1@gmail.com>
2018-04-20 18:13:08 +0000

    updating files to match drupaldelphia demp

--- js/photos.js
+++ js/photos.js
@@ -1,10 +1,17 @@
 /**
  * Created by lmiller on 4/18/18.
  */
+ 
+
 angular.module('demo', [])
     .controller('Photos', function($scope, $http) {
-        $http.get('https://jsonplaceholder.typicode.com/photos').
-        then(function(response) {
-            $scope.photos = response.data;
-        });
-    });
+        $http.get('https://drupal7dev-ldpm.c9users.io/articles.json').
+        success(function(data) {
+            $scope.photos = data;
+            console.log(data);
+        }).
+        error(function (data) {
+    $scope.data = "Request failed";
+    console.log("fail");
+  })
+});
